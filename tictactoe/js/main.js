2dc0fa5703193efffd6a9aeef305b6758c75bd58
Antonio Rhee <antoniolrhee@gmail.com>
2016-10-03 01:43:36 -0700

    game without win logic

--- tictactoe/js/main.js
+++ tictactoe/js/main.js
@@ -6,23 +6,56 @@ var oplayer = 'http://www.iconsdb.com/icons/preview/green/circle-outline-xxl.png
 var cells = document.getElementsByClassName('cell');
 var changeText = document.getElementById('playerTurn');
 var counter = 2;
+var winningCombinations = [[0,1,2],[3,4,5],[6,7,8],
+[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+var OMoves = [];
+var XMoves = [];
 
-// every click will add +1 to the counter and change clicked div background to x/o
-if(counter === 2) {
-  for(var i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', function() {
+// failed code
+// if(counter % 2 === 0) {
+//   for(var i = 0; i < cells.length; i++) {
+//     cells[i].addEventListener('click', function() {
+//       this.style.backgroundImage = 'url(' + xplayer + ')';
+//       changeText.innerHTML = "It is O's turn";
+//       counter++;
+//       console.log(counter);
+//     })
+//   }
+// }
+// else {
+//   for(var i = 0; i < cells.length; i++) {
+//     cells[i].addEventListener('click', function() {
+//       this.style.backgroundImage = 'url(' + oplayer + ')';
+//       changeText.innerHTML = "It is X's turn";
+//       counter++;
+//       console.log(counter);
+//     })
+//   }
+// }
+
+
+for(var i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', function() {
+    if(counter % 2 === 0) {
       this.style.backgroundImage = 'url(' + xplayer + ')';
       changeText.innerHTML = "It is O's turn";
       counter++;
-    })
-  }
-}
-else if(counter === 4) {
-  for(var i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', function() {
+      console.log(counter);
+      // XMoves.push();
+      // checkForWin(XMoves, "X");
+    }
+    else {
       this.style.backgroundImage = 'url(' + oplayer + ')';
       changeText.innerHTML = "It is X's turn";
       counter++;
-    })
-  }
+      console.log(counter);
+      // OMoves.push();
+      // checkForWin(OMoves, "O");
+    }
+    if(counter >= 11) {
+      alert("It's a draw!")
+    }
+  })
 }
+
+// function checkForWin(movesArray, name)
